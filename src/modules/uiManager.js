35f7f209d3b2f760a563db505c9085e39dbd1206
yyueda <yyuedaa@gmail.com>
2024-11-20 17:03:15 +0800

    feat: basic html structure

--- src/modules/uiManager.js
+++ src/modules/uiManager.js
@@ -1,8 +1,38 @@
 const UI = (function () {
     
     const initialiseUI = () => {
+        createHomePage();
         console.log("Test");
     };
+
+    const createHomePage = () => {
+        const body = document.querySelector("body");
+        const app = document.createElement("div");
+        const sidebar = createSidebar();
+        const content = createMainContent();
+
+        app.classList.add("app");
+
+        app.appendChild(sidebar);
+        app.appendChild(content)
+        body.append(app);
+    }
+
+    const createSidebar = () => {
+        const sidebar = document.createElement("div");
+        const projectsContainer = document.createElement("div");
+
+        sidebar.classList.add("sidebar");
+
+        return sidebar;
+    }
+
+    const createMainContent = () => {
+        const content = document.createElement("div");
+        content.classList.add("content");
+
+        return content;
+    }
 
     return { initialiseUI };
 })();
