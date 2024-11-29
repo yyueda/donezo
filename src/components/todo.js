dd9b745a065e6a5ea46f3c0b9532c2eadc666622
yyueda <yyuedaa@gmail.com>
2024-11-29 15:49:53 +0800

    style: Add edit button to task item

--- src/components/todo.js
+++ src/components/todo.js
@@ -195,7 +195,7 @@ const Todo = (function () {
         const todoButton = document.createElement("button");
         todoButton.classList.add("todo-button");
         const buttonBorder = document.createElement("span");
-        const svgElement = `
+        const tickSvg = `
             <svg width="20px" height="20px" viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -213,7 +213,7 @@ const Todo = (function () {
         });
 
         todoButton.addEventListener("mouseenter", () => {
-            buttonBorder.innerHTML = svgElement;
+            buttonBorder.innerHTML = tickSvg;
         });
 
         todoButton.addEventListener("mouseleave", () => {
@@ -257,13 +257,31 @@ const Todo = (function () {
             todoButton.classList.add("medium-priority");
         }
 
+        // Edit Button
+        const editButton = document.createElement("button");
+        editButton.classList.add("edit-btn");
+        const editSvg = `
+        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"/>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
+            <g id="SVGRepo_iconCarrier"> 
+                <path d="M20.1497 7.93997L8.27971 19.81C7.21971 20.88 4.04971 21.3699 3.27971 20.6599C2.50971 19.9499 3.06969 16.78 4.12969 15.71L15.9997 3.84C16.5478 3.31801 17.2783 3.03097 18.0351 3.04019C18.7919 3.04942 19.5151 3.35418 20.0503 3.88938C20.5855 4.42457 20.8903 5.14781 20.8995 5.90463C20.9088 6.66146 20.6217 7.39189 20.0997 7.93997H20.1497Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M21 21H12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> 
+            </g>
+        </svg>
+        `;
+        editButton.innerHTML = editSvg;
+        editButton.addEventListener("click", (e) => {
+            e.preventDefault();
+        })
+
         todoContent.appendChild(todoTitle);
         todoContent.appendChild(todoDesc);
         todoContent.appendChild(todoDueDate);
-        // todoContent.appendChild(todoPriority);
 
         todoDiv.appendChild(todoButton);
         todoDiv.appendChild(todoContent);
+        todoDiv.appendChild(editButton);
 
         return todoDiv;
     };
